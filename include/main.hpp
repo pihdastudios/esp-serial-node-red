c3f6bf51f044eff553e002b0a25acb2e4efe446e
Sid <3923297-igdoma@users.noreply.gitlab.com>
2021-10-25 15:12:51 +0700

    add wifi

--- include/main.hpp
+++ include/main.hpp
@@ -4,12 +4,14 @@
 #include <iomanip>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
+#include <freertos/event_groups.h>
 #include "esp_system.h"
 #include "esp_event.h"
 #include "nvs_flash.h"
 #include "driver/gpio.h"
 #include "driver/uart.h"
 #include "esp_vfs_dev.h"
+#include "esp_wifi.h"
 
 #include "cJSON.h"
 
@@ -19,10 +21,29 @@
 #define DHT_PIN GPIO_NUM_4
 #define BTN_PIN GPIO_NUM_5
 
+#define EXAMPLE_ESP_WIFI_SSID "null"
+#define EXAMPLE_ESP_WIFI_PASS "027412345678"
+#define EXAMPLE_ESP_MAXIMUM_RETRY 5
+
+/* FreeRTOS event group to signal when we are connected*/
+static EventGroupHandle_t s_wifi_event_group;
+
+/* The event group allows multiple bits for each event, but we only care about two events:
+ * - we are connected to the AP with an IP
+ * - we failed to connect after the maximum amount of retries */
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT      BIT1
+
 TaskHandle_t ISR = nullptr;
 static bool led_status = false;
 
 static SemaphoreHandle_t mutex;
+
+static void event_handler(void *arg, esp_event_base_t event_base,
+                          int32_t event_id, void *event_data);
+
+
+static int s_retry_num = 0;
 
 void DHT_task(void *pvParameter);
 void IRAM_ATTR button_isr_handler(void *pvParameter);
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,35 @@
 #include "main.hpp"
 
+static void event_handler(void *arg, esp_event_base_t event_base,
+                          int32_t event_id, void *event_data)
+{
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START)
+    {
+        esp_wifi_connect();
+    }
+    else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED)
+    {
+        if (s_retry_num < EXAMPLE_ESP_MAXIMUM_RETRY)
+        {
+            esp_wifi_connect();
+            s_retry_num++;
+            // ESP_LOGI(TAG, "retry to connect to the AP");
+        }
+        else
+        {
+            xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+        }
+        // ESP_LOGI(TAG,"connect to the AP fail");
+    }
+    else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP)
+    {
+        // ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
+        // ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+        s_retry_num = 0;
+        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+    }
+}
+
 void DHT_task(void *pvParameter)
 {
     DHT dht;
@@ -98,9 +128,45 @@ void app_main()
 
     // Enable cin uart
     esp_vfs_dev_uart_use_driver(0);
-    ESP_ERROR_CHECK(uart_driver_install((uart_port_t)CONFIG_ESP_CONSOLE_UART_NUM,
-                                        256, 0, 0, nullptr, 0));
+    uart_driver_install((uart_port_t)CONFIG_ESP_CONSOLE_UART_NUM,
+                        256, 0, 0, nullptr, 0);
 
+    esp_netif_init();
+
+    esp_event_loop_create_default();
+    esp_netif_create_default_wifi_sta();
+
+    esp_event_handler_instance_t instance_any_id;
+    esp_event_handler_instance_t instance_got_ip;
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    wifi_config_t wifi_config = {};
+    strcpy((char *)wifi_config.sta.ssid, EXAMPLE_ESP_WIFI_SSID);
+    strcpy((char *)wifi_config.sta.password, EXAMPLE_ESP_WIFI_PASS);
+    wifi_config.sta.threshold.authmode = WIFI_AUTH_WPA2_PSK;
+    wifi_config.sta.pmf_cfg.capable = true;
+    wifi_config.sta.pmf_cfg.required = false;
+
+    esp_event_handler_instance_register(WIFI_EVENT,
+                                                        ESP_EVENT_ANY_ID,
+                                                        &event_handler,
+                                                        NULL,
+                                                        &instance_any_id);
+    esp_event_handler_instance_register(IP_EVENT,
+                                                        IP_EVENT_STA_GOT_IP,
+                                                        &event_handler,
+                                                        NULL,
+                                                        &instance_got_ip);
+
+    esp_wifi_set_mode(WIFI_MODE_STA);
+    esp_wifi_set_config(WIFI_IF_STA, &wifi_config);
+    esp_wifi_start();
+
+    esp_event_handler_instance_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, instance_got_ip);
+    esp_event_handler_instance_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, instance_any_id);
+    vEventGroupDelete(s_wifi_event_group);
     //Create and start stats task
     xTaskCreate(button_task, "button_task", 4096, nullptr, 20, &ISR);
     xTaskCreate(&DHT_task, "DHT_task", 2048, nullptr, 5, nullptr);
